2f300ab35ab3f4cc33284249077d084204ea7343
Yohenba Kshetrimayum <roddik12kshetri@gmail.com>
2021-02-05 22:16:30 +0530

    linkedin-clone-complete

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './Widgets.css';
 import InfoIcon from '@material-ui/icons/Info';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 function Widgets() {
+
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets__aritcle">
+            <div className="widgets__aritcleLeft">
+                <FiberManualRecordIcon/>
+            </div>
+
+            <div className="widgets__aritcleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    );
+
     return (
         <div className="widgets">
             <div className="widgets__header">
                 <h2>LinkedIn News</h2>
                 <InfoIcon/>
             </div>
+
+            {newsArticle("i am back the world will change","i will destroy and take over everything")}
+            {newsArticle("I am back the world will change","i will destroy and take over everything")}
+            {newsArticle("I am back the world will change","i will destroy and take over everything")}
+            {newsArticle("I am back the world will change","i will destroy and take over everything")}
+            {newsArticle("I am back the world will change","i will destroy and take over everything")}
+            {newsArticle("I am back the world will change","i will destroy and take over everything")}
         </div>
     )
 }
